a8edd912e03966ddc7b65427cf9cd978f16be0fe
Tony Kieu <tkieu87@gmail.com>
2015-10-30 16:21:29 -0400

    Initial Commit

---
+++ eComBid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eComBid
+//
+//  Created by Tony Kieu on 10/30/15.
+//  Copyright © 2015 ECOMBID, INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
